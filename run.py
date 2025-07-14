606e50baaca5bce1c5c763cfb005dc34798dcab3
Tanvi Suryawanshi <tanvisuryawanshi0@gmail.com>
2025-07-14 11:20:02 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,35 @@
+import os
+import cv2     # type: ignore
+
+list_of_names = []
+
+
+def delete_old_data():
+   for i in os.listdir("generated-certificates/"):
+      os.remove("generated-certificates/{}".format(i))
+
+
+def cleanup_data():
+   with open('name-data.txt') as f:
+      for line in f:
+          list_of_names.append(line.strip())
+
+
+def generate_certificates():
+
+   for index, name in enumerate(list_of_names):
+      certificate_template_image = cv2.imread("Horizon-Certificate1.png")
+      cv2.putText(certificate_template_image, name.strip(), (690,690), cv2.FONT_HERSHEY_SIMPLEX,5, (0, 0, 250), 5, cv2.LINE_AA)
+      cv2.imwrite("generated-certificates/{}.jpg".format(name.strip()), certificate_template_image)
+      print("Processing {} / {}".format(index + 1,len(list_of_names)))
+      
+def main():
+   delete_old_data()
+   cleanup_data()
+   generate_certificates()
+
+
+
+if __name__ == '__main__':
+   main()
+
